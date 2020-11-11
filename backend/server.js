ced87ff1ce684116b627cbda1a84bcc427c2a85b
tweneboah <twentekghana@gmail.com>
2020-11-11 16:01:48 +0800

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,8 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, () => console.log('Server is runing'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server is runing in ${process.env.NODE_ENV} on port ${PORT}`)
+);
