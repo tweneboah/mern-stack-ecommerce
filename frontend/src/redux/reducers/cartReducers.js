450aa1c97f9e481cc7dc6b4fdf80a24df05556b3
tweneboah <twentekghana@gmail.com>
2020-11-14 11:15:44 +0800

    cart#4 - Cart component

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -1,15 +1,58 @@
-import { CART_ADD_ITEM } from '../actionTypes/cartActionTypes';
+// import { CART_ADD_ITEM } from '../actionTypes/cartActionTypes';
 
-export const cartReducer = (state = { cartItems: [] }, action) => {
+// export const cartReducer = (state = { cartItems: [] }, action) => {
+//   switch (action.type) {
+//     case CART_ADD_ITEM:
+//       const item = action.payload; //This contains all our item so we will check if it's already in our cart
+
+//       //state.cartItems = already product in our state
+//       //action.payload = current product
+//       const existItem = state.cartItems.find(x => x.product === item);
+
+//       if (existItem) {
+//         return {
+//           ...state,
+//           cartItems: state.cartItems.map(x =>
+//             x.product === existItem.product ? item : x
+//           ),
+//         };
+//       } else {
+//         return {
+//           ...state,
+//           cartItems: [...state.cartItems, item],
+//         };
+//       }
+
+//     default:
+//       return state;
+//   }
+// };
+
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+  CART_CLEAR_ITEMS,
+} from '../actionTypes/cartActionTypes';
+
+//Remember that we pass initialstate from localstorage so we have it here so we will check with the new data coming as a payload if it's already exist
+
+export const cartReducer = (
+  state = { cartItems: [], shippingAddress: {} },
+  action
+) => {
+  console.log(state);
   switch (action.type) {
     case CART_ADD_ITEM:
-      const item = action.payload; //This contains all our item so we will check if it's already in our cart
+      //Incoming data
+      const item = action.payload;
+      //state.cartItems = already existed data
 
-      //state.cartItems = already product in our state
-      //action.payload = current product
-      const existItem = state.cartItems.find(x => x.product === item);
-
+      const existItem = state.cartItems.find(x => x.product === item.product);
+      //product is the id that's how we assign it in our action
       if (existItem) {
+        //We have to check to check if the product exist we will only keep the new one otherwise no
         return {
           ...state,
           cartItems: state.cartItems.map(x =>
@@ -17,12 +60,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           ),
         };
       } else {
+        // If it's new we will add it to the existing one
         return {
           ...state,
           cartItems: [...state.cartItems, item],
         };
       }
-
     default:
       return state;
   }
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,22 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, ListGroup, Form, Button, Card } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Form,
+  Button,
+  Card,
+  Image,
+} from 'react-bootstrap';
 import { addToCart } from '../redux/actions/cartActions';
 import Message from '../components/Message';
 
 //We need the id from match.params.id;
 //To get value from query string we will use location.search
 
-const CartScreen = ({ match, location, histroy }) => {
+const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
   const qty = location.search ? Number(location.search.split('=')[1]) : 1;
   const dispatch = useDispatch();
@@ -22,7 +30,94 @@ const CartScreen = ({ match, location, histroy }) => {
   const cart = useSelector(state => state.cart);
   const { cartItems } = cart;
 
-  return <div>cart</div>;
+  //checkout
+
+  const checkoutHandler = () => {
+    //redirect to login if it has a queryString of shipping
+    history.push('/login?redirect=shipping'); //This means if not login it will take you to login otherwise redirect to shipping
+  };
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty<Link>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map(item => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} fluid rounded alt={item.name} />
+                  </Col>
+
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+
+                  <Col md={2}>{item.price}</Col>
+                  <Col md={2}>
+                    {/* We want to be able to change the quantity from here */}
+
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={e =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }>
+                      {/* This will [...Array(product.countInStock).keys()] output 0,1,2,3 if the value is 4 */}
+                      {[...Array(item.countInStock).keys()].map(x => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button variant='danger' type='button'>
+                      remove
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>
+                Subtoal ({' '}
+                {cartItems.reduce((acc, curr) => {
+                  return acc + curr.qty;
+                }, 0)}
+                )
+              </h2>
+              {/* PRICE */}
+              GHS{' '}
+              {cartItems
+                .reduce((acc, curr) => acc + curr.qty * curr.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <Button
+              disabled={cartItems.length === 0}
+              className='btn-block'
+              onClick={checkoutHandler}>
+              Proceed to checkout
+            </Button>
+            <ListGroup.Item></ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
